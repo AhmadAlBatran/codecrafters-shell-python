3bd3166fbd0dda6eae586216df5cdf7934637167
AhmadAlBatran <1AhmadBatran@gmail.com>
2026-06-30 10:57:28 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ import sys
 
 
 def main():
+    sys.stdout.write("$ ")
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
 
     pass
