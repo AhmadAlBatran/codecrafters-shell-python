6719ad5203d787f83b245d944a3fa5f8c0d13f4a
AhmadAlBatran <1AhmadBatran@gmail.com>
2026-06-30 19:00:38 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,35 +1,38 @@
 import os
+import shutil
 import sys
 
-path = os.environ.get("PATH")
+commands = ["exit", "echo", "type"]
 
 
-def commandtype(command, builtin, path):
-
-    if command in builtin:
-        return "builtin"
+def handle_type(cmd):
+    if cmd in commands:
+        print(f"{cmd} is a shell builtin")
+    elif path := shutil.which(cmd):
+        print(f"{cmd} is {path}")
     else:
-        pass
+        print(f"{cmd}: not found")
+
+
+def handle_builtin(args):
+    match args[0]:
+        case "exit":
+            sys.exit(0)
+        case "echo":
+            txt = " ".join(args[1:])
+            print(txt)
+        case "type":
+            handle_type(args[1])
+        case _:
+            print(f"{args[0]}: command not found")
 
 
 def main():
-    builtin = ["echo", "type", "exit"]
-
     while True:
         sys.stdout.write("$ ")
-        command = input()
-
-        if command == "exit":
-            break
-        if command.startswith("echo"):
-            print(command[5:])
-            continue
-        if command.startswith("type"):
-            command = command[5:]
-            if command in builtin:
-                print(f"{command} is a shell builtin")
-                continue
-        print(f"{command}: not found")
+        command = input("")
+        arguments = command.split(" ")
+        handle_builtin(arguments)
 
 
 if __name__ == "__main__":
